82c48b1ca06aad0e7b6ea78c947e67afb7aee558
Pierre <pm4rtin@gmail.com>
2019-06-04 11:07:28 +0200

    version ok pour le web

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,6 @@ export class DataService {
   constructor(private http:HttpClient) { }
 
 getWeather(){
-  return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=84328ff6ad7bee862aa3ba60e70d7dde&units=metric&lang=fr')
+  return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=84328ff6ad7bee862aa3ba60e70d7dde&units=metric&lang=fr')
   }
 }
